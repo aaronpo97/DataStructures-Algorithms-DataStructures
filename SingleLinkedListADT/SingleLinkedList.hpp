7a76ffb17c2b5c544a2b4bf04d038075b17c5163
Aaron Po <apo2@uwo.ca>
2025-09-24 14:17:00 -0400

    Update SingleLinkedList

--- SingleLinkedListADT/SingleLinkedList.hpp
+++ SingleLinkedListADT/SingleLinkedList.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <cstddef>
+#include <utility>
 
 template <typename ValueType> class SingleLinkedList {
   private:
@@ -8,89 +10,175 @@ template <typename ValueType> class SingleLinkedList {
         Node(ValueType const &d = ValueType{}, Node *n = nullptr)
             : data{d}, next{n} {
         }
-
-        Node(ValueType &&d, Node *n = nullptr) : data{std::move(d)}, next(n) {
+        Node(ValueType &&d, Node *n = nullptr) : data{std::move(d)}, next{n} {
         }
     };
 
-    Node  *m_head;
-    size_t m_size;
+    Node  *m_head{nullptr};
+    size_t m_size{0};
 
   public:
     class const_iterator {
-
       protected:
-        Node *m_current;
-
-        const_iterator(Node *p) : m_current{p} {
+        Node *m_current{nullptr};
+        explicit const_iterator(Node *p) : m_current{p} {
         }
-
-        friend class SinglyLinkedList<ValueType>;
+        friend class SingleLinkedList;
 
       public:
-        const_iterator() : m_current{nullptr} {
-        }
+        const_iterator() = default;
+
+        // dereference
         ValueType const &operator*() const {
             return m_current->data;
         }
+        ValueType const *operator->() const {
+            return &m_current->data;
+        }
+
+        // advance
+        const_iterator &operator++() { // pre-increment
+            m_current = m_current ? m_current->next : nullptr;
+            return *this;
+        }
+        const_iterator operator++(int) { // post-increment
+            const_iterator tmp{*this};
+            ++(*this);
+            return tmp;
+        }
+
+        // compare
         bool operator==(const_iterator const &rhs) const {
             return m_current == rhs.m_current;
-        };
+        }
         bool operator!=(const_iterator const &rhs) const {
             return !(*this == rhs);
         }
-    }
+    };
 
     class iterator : public const_iterator {
       public:
-        iterator(Node *p = nullptr) : const_iterator(p) {
+        explicit iterator(Node *p = nullptr) : const_iterator(p) {
         }
-        Value_Type &operator*() {
-            return const_iterator::m_current->data;
+
+        ValueType &operator*() {
+            return this->m_current->data;
         }
+        ValueType *operator->() {
+            return &this->m_current->data;
+        }
+
         iterator &operator++() {
-            this->m_current = this->m_current->next;
+            this->m_current = this->m_current ? this->m_current->next : nullptr;
             return *this;
         }
+        iterator operator++(int) {
+            iterator tmp{*this};
+            ++(*this);
+            return tmp;
+        }
     };
 
-    SingleLinkedList() : m_head(nullptr), m_size(0) {};
-    SingleLinkedList(List const &rhs);
+    SingleLinkedList() = default;
+
+    // no copying allowed
+    SingleLinkedList(SingleLinkedList const &)            = delete;
+    SingleLinkedList &operator=(SingleLinkedList const &) = delete;
+
+    // move allowed
+    SingleLinkedList(SingleLinkedList &&rhs) noexcept
+        : m_head(rhs.m_head), m_size(rhs.m_size) {
+        rhs.m_head = nullptr;
+        rhs.m_size = 0;
+    }
+    SingleLinkedList &operator=(SingleLinkedList &&rhs) noexcept {
+        if (this != &rhs) {
+            clear();
+            m_head     = rhs.m_head;
+            m_size     = rhs.m_size;
+            rhs.m_head = nullptr;
+            rhs.m_size = 0;
+        }
+        return *this;
+    }
+
     ~SingleLinkedList() {
         clear();
-    };
-    List &operator=(List const &rhs);
-    SingleLinkedList(List &&rhs);
+    }
 
     iterator begin() {
-        return iterator(head);
+        return iterator(m_head);
     }
     const_iterator begin() const {
-        return const_iterator(head);
+        return const_iterator(m_head);
     }
     iterator end() {
         return iterator(nullptr);
-    };
-    const_iterator end() const{
+    }
+    const_iterator end() const {
         return const_iterator(nullptr);
-    };
+    }
 
-    size_t size() const { 
+    size_t size() const {
         return m_size;
-    };
-    bool   empty() const
-    {
+    }
+    bool empty() const {
         return m_size == 0;
     }
-        ;
 
-    void     push_front(ValueType const &x);
-    void     pop_front();
-    iterator insert_after();
-    iterator erase_after();
+    void push_front(ValueType const &x) {
+        m_head = new Node(x, m_head);
+        ++m_size;
+    }
+    void push_front(ValueType &&x) {
+        m_head = new Node(std::move(x), m_head);
+        ++m_size;
+    }
+
+    void pop_front() {
+        if (!m_head)
+            return;
+        Node *temp = m_head;
+        m_head     = m_head->next;
+        delete temp;
+        --m_size;
+    }
+
+    iterator insert_after(iterator itr, ValueType const &value) {
+        Node *current = itr.m_current;
+
+        if (!current)
+            return end();
+
+        Node *n       = new Node(value, current->next);
+        current->next = n;
+        
+        ++m_size;
+        return iterator(n);
+    }
+
+    iterator erase_after(iterator itr) {
+        Node *current = itr.m_current;
+        if (!current || !current->next)
+            return end();
+
+        std::cout << "inside erase_after\n";
+        Node *to_delete = current->next;
+        current->next   = to_delete->next;
+        delete to_delete;
+        
+        --m_size;
+        
+        return iterator(current->next);
+    }
 
     void clear() {
-        while (!empty())
-            pop_front();
+
+        while (m_head) {
+            Node *tmp = m_head->next;
+            delete m_head;
+            m_head = tmp;
+        }
+        m_size = 0;
     }
 };
--- SingleLinkedListADT/main.cpp
+++ SingleLinkedListADT/main.cpp
@@ -1,5 +1,58 @@
 #include <iostream>
 
+#include "SingleLinkedList.hpp"
+
+
 int main(int, char **) {
-    std::cout << "Hello world\n";
+    {
+        std::cout << "Testing the default constructor with std::string:\n";
+    SingleLinkedList<std::string> test_ll;
+        
+        std::cout << "\ttest_ll has size 0 when first constructed: " << std::boolalpha << (test_ll.size() == 0) << "\n";
+
+
+        std::cout << "\tPushing \"Hello\" to test_ll using .push_front()\n";
+        test_ll.push_front("Hello");   
+
+        std::cout << "\tPushing \"World\" to test_ll using "
+                     ".insert_after(std::begin(test_ll))\n";
+
+        SingleLinkedList<std::string>::iterator it = 
+            test_ll.insert_after(std::begin(test_ll), "world");
+
+
+        std::cout << "\tNow with the iterator to the \"world\"'s node, use "
+                     "insert_after to push a new string\n";
+
+        test_ll.insert_after(it, "Hi");
+
+        std::cout << "\tPrint all elements of the linked list using an iterator-based for loop:\n";
+
+        for (auto it = std::begin(test_ll); it != std::end(test_ll); ++it) {
+            std::cout << "\t\t- " << *it << '\n';
+        }
+
+        std::cout << "Remove the third element:\n";
+
+        size_t it_count{};
+        auto   iter = std::begin(test_ll);
+        size_t index_to_delete{3};
+        // get the index before index 3
+        while (it_count != (index_to_delete - 1)) {
+            ++iter;
+            ++it_count;
+        }
+
+    
+        std::cout << *iter << " " << it_count << "\n";
+
+        test_ll.erase_after(iter);
+      
+        
+        std::cout << test_ll.size();
+
+        
+
+    }
+
 }
